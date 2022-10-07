0353e0fc0556226b08c1a1947d3f19e47dd5c810
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-07 15:24:49 -0700

    create a employee model with setters and getters

---
+++ TodoApp/src/test/java/com/Qickzo/TodoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Qickzo.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
