7229343ddaf5bc0b5aaca354b8512d9c4f83bd03
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-07 17:26:08 -0700

    create an employee with id controller

--- TodoApp/src/main/java/com/Qickzo/TodoApp/controller/EmployeeController.java
+++ TodoApp/src/main/java/com/Qickzo/TodoApp/controller/EmployeeController.java
@@ -3,10 +3,13 @@ package com.Qickzo.TodoApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Qickzo.TodoApp.exeception.ResourceNotFoundException;
 import com.Qickzo.TodoApp.model.Employee;
 import com.Qickzo.TodoApp.repository.EmployeeRepository;
 
@@ -22,6 +25,16 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List <Employee> getAllEmployees(){
 		return employeerepsitory.findAll();
+	}
+	
+	//get employee by id
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee>getEmployeeById(@PathVariable Long id){
+		Employee employee = employeerepsitory.findById(id)
+				.orElseThrow(()->new ResourceNotFoundException("employee not exist with is :"+
+		id));
+		return ResponseEntity.ok(employee);
 	}
 
 }
